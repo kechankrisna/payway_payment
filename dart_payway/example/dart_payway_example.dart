a8c8ca6faa2fab1e9c478fb83666d9ca3caca4c7
kechankrisna <ke.chankrisna168@gmail.com>
2023-03-09 22:16:15 +0700

    init dart payway

---
+++ dart_payway/example/dart_payway_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_payway/dart_payway.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ dart_payway/lib/dart_payway.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_payway;
+
+export 'src/dart_payway_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
